06998449b2e2c68ce910102d3f55025f1b9cca5a
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-07-17 18:17:20 +0700

    Membuat tabel migrasi student dan major

---
+++ database/migrations/2025_07_17_111253_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index();
+            $table->integer('nim')->unique();
+            $table->date('birth_date');
+            $table->enum('gender', ['male', 'female']);
+            $table->text('address');
+            $table->foreignId('major_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
