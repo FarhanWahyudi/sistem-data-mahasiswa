107b8b8a4ce73773022396a99a632b6318625895
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-07-20 23:21:00 +0700

    Membuat major repository

---
+++ app/Repositories/MajorRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Major;
+
+class MajorRepository
+{
+    public function getAll()
+    {
+        return Major::withCount(['students as total_students'])->orderBy('name')->paginate(5);
+    }
+
+    public function getById(int $id)
+    {
+        return Major::with(['students:nim,name,major_id'])->findOrFail($id);
+    }
+
+    public function create(string $name)
+    {
+        return Major::create([
+            'name' => $name
+        ]);
+    }
+
+    public function update(int $id, string $name)
+    {
+        $major = Major::findOrFail($id);
+        $major->update([
+            'name' => $name
+        ]);
+        return $major;
+    }
+
+    public function delete(int $id)
+    {
+        $major = Major::findOrFail($id);
+        $major->delete();
+    }
+}
